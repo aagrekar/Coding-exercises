1134629693e2d3d72e861e044246da8fce6001b6
Ankush Agrekar <a.agrekar@gmail.com>
2017-10-09 00:06:52 -0500

    updates to consider floating coordinates

--- Integral Point inside triangle/code.py
+++ Integral Point inside triangle/code.py
@@ -4,28 +4,25 @@ def calc_area(x,y,vx1,vy1,vx2,vy2):
     c = ((vx2-vx1)**2+(vy2-vy1)**2)**0.5
     s = (a+b+c)/2
     
-    area=(s*(s-a)*(s-b)*(s-c))**0.5
-    return round(area,4)
+    area=(s*abs(s-a)*abs(s-b)*abs(s-c))**0.5
+    return area
 
 def find_min_point(x1,y1,x2,y2,x3,y3):
     points_distance = dict()
-    for x in range(min(x1,x2,x3),max(x1,x2,x3)+1):
-        for y in range(min(y1,y2,y3),max(y1,y2,y3)+1):
+    for x in range(min(round(x1),round(x2),round(x3)),max(round(x1),round(x2),round(x3))+1):
+        for y in range(min(round(y1),round(y2),round(y3)),max(round(y1),round(y2),round(y3))+1):
             coord = (x,y)
-            print(coord)
             a1 = calc_area(x,y,x1,y1,x2,y2)
             a2 = calc_area(x,y,x1,y1,x3,y3)
             a3 = calc_area(x,y,x2,y2,x3,y3)
             a = calc_area(x1,y1,x2,y2,x3,y3)
-            print(a1+a2+a3)
-            print(a)
-            if a1+a2+a3 == a:
+            if round(a1+a2+a3,4) == round(a,4):
                 dist = (x-x1)**2 + (y-y1)**2 + (x-x2)**2 + (y-y2)**2 + (x-x3)**2 + (y-y3)**2
                 points_distance.update({(x,y):dist})
             
     points_distance = sorted(points_distance.items(),key = lambda x:(x[1],x[0][0],x[0][1])
     print(points_distance)
-    if len(points_distance)>=1:
+    if len(points_distance)>0:
         return [coord for coord in points_distance[0][0]]
     else:
         return [None, None]
